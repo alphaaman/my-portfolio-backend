d2ecc165a923c98a0109464e6f2dc04036c7e5c4
alphaaman <gaman9473@gmail.com>
2023-07-11 21:33:59 +0530

    testing2

---
+++ virenv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\ADARSH\Downloads\django react\Portfolio-using-React-and-Django.github.io-main\my-portfolio-backend\virenv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
